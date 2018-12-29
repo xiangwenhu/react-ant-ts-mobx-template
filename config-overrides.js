068f69f4074788f9695c3410c0a6e3dc80e83c6d
TKOFFICE\huxw <huxw@mail.jj.cn>
2018-12-29 17:15:00 +0800

    修改readme

--- config-overrides.js
+++ config-overrides.js
@@ -1,35 +1,50 @@
-const workboxPlugin = require('workbox-webpack-plugin')
-const path = require('path')
+const workboxPlugin = require("workbox-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    webpack: function (config, env) {
-
-       if (env === "production") {
+    webpack: function(config, env) {
+        if (env === "production") {
             console.log("Production build - Adding Workbox for PWAs");
 
             const workboxConfigProd = {
-                swSrc: path.join(__dirname, 'src', 'custom-service-worker.js'),
-                swDest: 'custom-service-worker.js',
-                importWorkboxFrom: 'disabled'
-              }
-              // 删除默认的WorkboxWebpackPlugin配置
-              config = removePreWorkboxWebpackPluginConfig(config)
-             // 加入我们的配置
-              config.plugins.push(new workboxPlugin.InjectManifest(workboxConfigProd))
-       }
+                swSrc: path.join(__dirname, "src", "custom-service-worker.js"),
+                swDest: "custom-service-worker.js",
+                importWorkboxFrom: "disabled"
+            };
+            // 删除默认的WorkboxWebpackPlugin配置
+            config = removePreWorkboxWebpackPluginConfig(config);
+            // 加入我们的配置
+            config.plugins.push(
+                new workboxPlugin.InjectManifest(workboxConfigProd)
+            );
+        }
+
+        // 定义变量
+        rewireDefinePlugin(config, env, {
+            "$$ENV": JSON.stringify(env)
+        });
 
         return config;
     }
-}
+};
 
+function rewireDefinePlugin(config, env, definePluginOptions = {}) {
+    // Add the define plugin to the list of plugins
+    config.plugins = (config.plugins || []).concat([
+        new webpack.DefinePlugin(definePluginOptions)
+    ]);
+
+    return config;
+}
 
 // 此函数用来找出 默认配置中的 WorkboxWebpackPlugin， 并把它删除
 function removePreWorkboxWebpackPluginConfig(config) {
-    const preWorkboxPluginIndex = config.plugins.findIndex((element) => {
-        return Object.getPrototypeOf(element).constructor.name === 'GenerateSW'
-    })
+    const preWorkboxPluginIndex = config.plugins.findIndex(element => {
+        return Object.getPrototypeOf(element).constructor.name === "GenerateSW";
+    });
     if (preWorkboxPluginIndex !== -1) {
-        config.plugins.splice(preWorkboxPluginIndex, 1)
+        config.plugins.splice(preWorkboxPluginIndex, 1);
     }
-    return config
+    return config;
 }
